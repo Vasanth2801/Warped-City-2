48d5b4760959b59c850930bc149f093209382e93
Vasanth2801 <vasanthravichandran28@gmail.com>
2026-04-17 16:58:27 +0530

    Parallax Manager Script

--- Warped City 2/Assets/Scripts/UI/ParallaxManager.cs
+++ Warped City 2/Assets/Scripts/UI/ParallaxManager.cs
@@ -5,13 +5,29 @@ public class ParallaxManager : MonoBehaviour
     [System.Serializable]
     public class ParallaxLayer
     {
-        public Transform layerTransform;
         [Range(0, 1)] public float parallaxFactor;
+        public Transform layerTransform;
     }
 
     public ParallaxLayer[] layers;
     [SerializeField] private Transform cameraTransform;
     [SerializeField] private Vector3 lastCameraPosition;
 
+    void Start()
+    {
+        lastCameraPosition = cameraTransform.position;
+    }
 
+    void LateUpdate()
+    {
+        Vector3 delta = cameraTransform.position - lastCameraPosition;
+
+        foreach (ParallaxLayer layer in layers)
+        {
+            float parallaxX = delta.x * layer.parallaxFactor;
+            float parallaxY = delta.y * layer.parallaxFactor;
+            layer.layerTransform.position = new Vector3(parallaxX, parallaxY, 0);
+        }
+        lastCameraPosition = cameraTransform.position;
+    }
 }
